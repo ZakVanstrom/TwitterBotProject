2ca8c199f5ff44ef60bbc5669355a1f0ccf0bae2
Zak Vanstrom <zvanstro@asu.edu>
2020-08-24 13:53:01 -0700

    added docker distro images

---
+++ dockerImage/bot/py/getPhotos.sh
@@ -0,0 +1,7 @@
+cd /path to directory || exit\
+rm ./* \
+icloudpd --directory ./ \
+    --username zakyre15@yahoo.com \
+    --password MarenMagic85! \
+    --recent 50 \
+    --live-photo-size original \
